17ac28c00ea50f961aeaf2d72dd398e3e1312b64
anna-masl <anna.maslinnikova@gmail.com>
2025-06-15 23:59:25 +0300

    add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let numOrStr = prompt('input number or string');
 console.log(numOrStr)
 
+/* second code
 switch (numOrStr?.trim() && isNaN(+numOrStr)) {
     case undefined:
         console.log('ви скасували');
@@ -14,5 +15,22 @@ switch (numOrStr?.trim() && isNaN(+numOrStr)) {
     default:
         console.log('OK!');
     break;
-
 }
+*/
+
+switch (true){
+    case numOrStr === null:
+       console.log('ви скасували');
+    break;
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+    break;
+    case isNaN(+numOrStr):
+        console.log(' number is Ba_NaN');
+    break;
+    default:
+        console.log('OK!');
+    break;
+}
+
+
